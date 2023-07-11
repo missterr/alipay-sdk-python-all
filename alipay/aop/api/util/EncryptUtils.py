d61bec8a19f46048fca0ba392fe2a0febf7751ae
Maksim Kopytov <misst3rr@gmail.com>
2023-07-11 16:53:53 +0100

    try to fix

--- alipay/aop/api/util/EncryptUtils.py
+++ alipay/aop/api/util/EncryptUtils.py
@@ -29,7 +29,7 @@ def aes_encrypt_content(content, encrypt_key, charset):
     else:
         length = len(bytes(content))
     padded_content = pad(content, length)
-    iv = '\0' * BLOCK_SIZE
+    iv = b'\0' * BLOCK_SIZE
     cryptor = AES.new(base64.b64decode(encrypt_key), AES.MODE_CBC, iv)
     encrypted_content = cryptor.encrypt(padded_content)
     encrypted_content = base64.b64encode(encrypted_content)
